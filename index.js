8f29f51875bfa02b1b5af1e32c6dede43811bc72
Cathy Lii <cathy.y.lii@gmail.com>
2018-04-19 17:23:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ function happyHolidaysTo(name) {
 return `Happy holidays, ${name}`; 
 }
 
-happyHolidaysTo(you)
+happyHolidaysTo("you")
 
 function happyHolidayTo(holiday,name) {
 return `Happy ${holiday}, ${name}!`;
 }
+
+happyHolidayTo("Independence Day", "you")
 
 function holidayCountdown(holiday,days) {
 return `It\'s ${days} days until ${holiday}!`;
